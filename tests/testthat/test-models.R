d151affe1d74802b393e62f2bf703be7d0339074
Nacho <nacho@air>
2016-12-01 18:29:07 -0500

    Change names of tests, probably not working

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -1,28 +1,28 @@
 context("Models")
 
 test_that("integrated model works", {
-    out <- integrated_model(pars = c(1,1,1,1), 
-                            Tlevel1 = rnorm(10), 
-                            Tlevel2 = rnorm(10), 
-                            mean_Tlevel1 = 0, 
+    out <- integrated.model(pars = c(1,1,1,1),
+                            Tlevel1 = rnorm(10),
+                            Tlevel2 = rnorm(10),
+                            mean_Tlevel1 = 0,
                             sd_Tlevel1 = 1)
   expect_equal(TRUE, is.numeric(out))
 })
 
 test_that("niche model works", {
-  out <- niche_model(pars = c(1,1,1,1), 
-                          Tlevel1 = rnorm(10), 
-                          Tlevel2 = rnorm(10), 
-                          mean_Tlevel1 = 0, 
+  out <- niche_model(pars = c(1,1,1,1),
+                          Tlevel1 = rnorm(10),
+                          Tlevel2 = rnorm(10),
+                          mean_Tlevel1 = 0,
                           sd_Tlevel1 = 1)
   expect_equal(TRUE, is.numeric(out))
 })
 
 test_that("neutral model works", {
-  out <- neutral_model(pars = NULL, 
-                          Tlevel1 = rnorm(10), 
-                          Tlevel2 = rnorm(10), 
-                          mean_Tlevel1 = 0, 
+  out <- neutral.model(pars = NULL,
+                          Tlevel1 = rnorm(10),
+                          Tlevel2 = rnorm(10),
+                          mean_Tlevel1 = 0,
                           sd_Tlevel1 = 1)
   expect_equal(TRUE, is.numeric(out))
 })
--- tests/testthat/test-plot_pred.R
+++
@@ -1,9 +0,0 @@
-context("Plot")
-
-test_that("plot it works", {
-  out <- plot_pred( 
-                pars = c(1,1,1,1), 
-                Tlevel1 = rnorm(10), 
-                Tlevel2 = rnorm(10)) 
-  expect_equal(TRUE, out$xpd)
-})
