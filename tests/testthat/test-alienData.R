2427cf4e7557c5a9e5b3e7159792fa1801457f39
Steve Vissault <s.vissault@yahoo.fr>
2017-02-17 15:44:51 -0500

    add unit tests to data structure issue #18

--- tests/testthat/test-alienData.R
+++ tests/testthat/test-alienData.R
@@ -1,13 +1,13 @@
 context("alienData function")
 
-# load('./argsAlienData.RData')
-# out <- as.alienData(idObs=idObs,interactPair=interactPair,traitSp=traitSp,traitInd=traitInd,verbose=FALSE)
-#
-# test_that("check data structure", {
-#   expect_is(out, "alienData")
-#   # Even if items from the list are NULL, all items have to be returned
-#   expect_equal(names(out),c("idObs","interactSp","interactInd","coOcc","coAbund","siteEnv","traitSp","traitInd","phylo"))
-# })
+load('./argsAlienData.RData')
+out <- as.alienData(idObs=idObs,interactPair=interactPair,traitSp=traitSp,traitInd=traitInd,verbose=FALSE)
+
+test_that("check data structure", {
+  expect_is(out, "alienData")
+  # Even if items from the list are NULL, all items have to be returned
+  expect_equal(names(out),c("idObs","interactSp","interactInd","coOcc","coAbund","siteEnv","traitSp","traitInd","phylo"))
+})
 
 test_that("check data integrity", {
 
