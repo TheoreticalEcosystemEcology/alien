f6d998498016e0b4e977463a4905322436e87ccc
ibartomeus <ibartomeus@home>
2017-02-17 16:49:28 +0100

    Add comments on as.alienData posible enhancements

--- Test_as.aleinData.R
+++ Test_as.aleinData.R
@@ -4,3 +4,12 @@ library("alienR")
 load("data/argsAlienData.RData") # How I created this file is explained in the explnation.Rmd. File is in attachment
 dataBartomeus <- as.alienData(idObs=idObs,interactPair=interactPair,traitInd=traitInd,traitSp=traitSp)
 str(dataBartomeus)
+
+head(dataBartomeus$idObs)
+head(dataBartomeus$interactSp) #being this a bipartite netw, you don't want to have a square matrix here
+dim(dataBartomeus$interactSp)
+head(dataBartomeus$interactInd)
+dim(dataBartomeus$interactInd) #same here, this is quite a waste of space, because most indiv won't interact with others, and certainly not with themselves
+head(dataBartomeus$coOcc) #this I think is not well calculated. SHould be from idObs
+head(dataBartomeus$traitSp)
+head(dataBartomeus$traitInd)
