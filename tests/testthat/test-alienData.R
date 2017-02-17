8a9ae0306594d0e33bcea0922c8c049b35044dcd
Steve Vissault <s.vissault@yahoo.fr>
2017-02-17 12:04:26 -0500

    init test on integrity #18

--- tests/testthat/test-alienData.R
+++ tests/testthat/test-alienData.R
@@ -5,7 +5,12 @@ out <- as.alienData(idObs=idObs,interactPair=interactPair,traitSp=traitSp,traitI
 
 test_that("check data structure", {
   expect_is(out, "alienData")
+  # Even if items from the list are NULL, all items have to be returned
+  expect_equal(names(out),c("idObs","interactSp","interactInd","coOcc","coAbund","siteEnv","traitSp","traitInd","phylo"))
 })
 
 test_that("check data integrity", {
+  # create fake data
+  idObs <- data.frame(idSite=rep(3,c("")), idTime=NULL)
+
 })
