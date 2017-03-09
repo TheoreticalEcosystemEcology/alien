bb104e208e4dd2bba57cc45d18cf203a2c2082ff
Steve Vissault <s.vissault@yahoo.fr>
2017-03-09 14:37:15 -0500

    set formula, level and generalized function

--- R/fitDirectCentrality.R
+++ R/fitDirectCentrality.R
@@ -7,21 +7,21 @@
 #' @param data a dataframe with a first column named I (the interaction) and extra columns with covariates (can be named at will)
 #' @param specify the algorithm: one of the `stats` family or the classification algorithm `randomForest`. Default is binomial.
 #' @param specify the ecological level on which the method is applied. Choices are 'individus' or 'species'. Default is species.
+#' @param set the formula passed to the algorithm (see `family`).
+#' @param specificy the traits to include in the algorithm
 #' @param ... other params passed to the glm or randomForest algorithm
-#' @exemple fitDirectCentrality(bartomeus,family="poisson",level="species")
+#' @examples fitDirectCentrality(bartomeus,family='poisson',level='species')
 #' @author
-#' Dominique Gravel
+#' Dominique Gravel & Steve Vissault
 #'
 #' @references
-#' Poisot T., Cirtwill A. R., Cazelles A. R., Cazelles K., Gravel D., Fortin M.-J. and Stouffer D.B. (2015). The structure of probabilistic networks. \emph{bioRxiv 016485}; doi: https://doi.org/10.1101/016485.
+#' Rohr
 #'
 #' @export
-fitDirectCentrality <- function(data, family = "binomial", level = "species", ...) {
+fitDirectCentrality <- function(data, family = "binomial", level = "species", formula = "I ~ . * .", traits = NULL, ...) {
 
     if (class(data) != "alienData") {
-
         stop("`data` arg has to be alienData class")
-
     }
 
     if (level == "species") {
@@ -29,41 +29,9 @@ fitDirectCentrality <- function(data, family = "binomial", level = "species", ..
             stop("traitSp and interactSp are absent from data. Both are to be provided to fit the algorithm at the individu level")
         }
 
-        # set I
-        interactSp <- data.frame(expand.grid(rownames(data$interactSp), colnames(data$interactSp)),
-            I = as.vector(data$interactSp))
-        names(interactSp)[1:2] <- c("spFrom", "spTo")
-
-        # drop NA
-        interactSp <- interactSp[which(!is.na(interactSp$I)), ]
-
-        # cast trait
-        traitSp <- reshape2::dcast(data = data$traitSp, idSp ~ traitName, value.var = "value")
-
-        # get covariates for idFrom
-        interactSp <- merge(interactSp, traitSp, all.x = TRUE, by.x = "spFrom", by.y = "idSp")
-        colnames(interactSp)[4:ncol(interactSp)] <- paste("spFrom_", colnames(interactSp)[4:ncol(interactSp)],
-            sep = "")
-
-        # get covariates for idTo
-        nc <- ncol(interactSp)
-        interactSp <- merge(interactSp, traitSp, all.x = TRUE, by.x = "spTo", by.y = "idSp")
-        colnames(interactSp)[(nc + 1):ncol(interactSp)] <- paste("spTo_", colnames(interactSp)[(nc +
-            1):ncol(interactSp)], sep = "")
-
-        # remove columns containing all NA (no species match to the traits)
-        interactSp <- interactSp[, colSums(is.na(interactSp)) < nrow(interactSp)]
-
-        # subset df to get only I and covariates (traits)
-        interactSp <- interactSp[, -c(1:2)]
-
-        # guess and cast each columns in the right data types
-        interactSp <- data.frame(I = interactSp[, 1], as.data.frame(sapply(interactSp[,
-            -c(1)], type.convert)))
-
-        # set data to pass through model
-        intertrait <- interactSp
-
+        df_trait <- data$traitSp
+        df_interact <- data$interactSp
+        id <- "idSp"
     }
 
     if (level == "individus") {
@@ -72,48 +40,55 @@ fitDirectCentrality <- function(data, family = "binomial", level = "species", ..
             stop("traitInd and interactInd are absent from data. Both are to be provided to fit the algorithm at the individu level")
         }
 
-        # set I
-        interactInd <- data.frame(expand.grid(rownames(data$interactInd), colnames(data$interactInd)),
-            I = as.vector(data$interactInd))
-        names(interactInd)[1:2] <- c("indFrom", "indTo")
-
-        # drop NA
-        interactInd <- interactInd[which(!is.na(interactInd$I)), ]
-
-        # cast trait
-        traitInd <- reshape2::dcast(data = data$traitInd, idInd ~ traitName, value.var = "value")
-
-        # get covariates for idFrom
-        interactInd <- merge(interactInd, traitInd, all.x = TRUE, by.x = "indFrom", by.y = "idInd")
-        colnames(interactInd)[4:ncol(interactInd)] <- paste("indFrom_", colnames(interactInd)[4:ncol(interactInd)],
-            sep = "")
-
-        # get covariates for idTo
-        nc <- ncol(interactInd)
-        interactInd <- merge(interactInd, traitInd, all.x = TRUE, by.x = "indTo", by.y = "idInd")
-        colnames(interactInd)[(nc + 1):ncol(interactInd)] <- paste("indTo_", colnames(interactInd)[(nc +
-            1):ncol(interactInd)], sep = "")
-
-        # remove columns containing all NA (no species match to the traits)
-        interactInd <- interactInd[, colSums(is.na(interactInd)) < nrow(interactInd)]
-
-        # subset df to get only I and covariates (traits)
-        interactInd <- interactInd[, -c(1:2)]
-
-        # guess and cast each columns in the right data types
-        interactInd <- data.frame(I = interactInd[, 1], as.data.frame(sapply(interactInd[,
-            -c(1)], type.convert)))
-
-        # set data to pass through model
-        intertrait <- interactInd
-
+        df_trait <- data$traitInd
+        df_interact <- data$interactInd
+        id <- "idInd"
     }
 
+    # set I
+    df_interact <- data.frame(expand.grid(rownames(df_interact), colnames(df_interact)), I = as.vector(df_interact))
+    names(df_interact)[1:2] <- c("idFrom", "idTo")
+
+    # drop NA
+    df_interact <- df_interact[which(!is.na(df_interact$I)), ]
+
+    # cast df_trait
+    df_trait <- reshape2::dcast(data = df_trait, id ~ traitName, value.var = "value")
+
+    # select df_trait
+    if(!is.null(traits)){
+      df_trait <- subset(df_trait, traitName %in% traits)
+    }
+
+    # get covariates for idFrom
+    df_interact <- merge(df_interact, df_trait, all.x = TRUE, by.x = "idFrom", by.y = id)
+    colnames(df_interact)[4:ncol(df_interact)] <- paste("idFrom_", colnames(df_interact)[4:ncol(df_interact)],
+        sep = "")
+
+    # get covariates for idTo
+    nc <- ncol(df_interact)
+    df_interact <- merge(df_interact, df_trait, all.x = TRUE, by.x = "idTo", by.y = id)
+    colnames(df_interact)[(nc + 1):ncol(df_interact)] <- paste("idTo_", colnames(df_interact)[(nc +
+        1):ncol(df_interact)], sep = "")
+
+    # remove columns containing all NA (no species match to the traits)
+    df_interact <- df_interact[, colSums(is.na(df_interact)) < nrow(df_interact)]  # TODO: Check
+
+
+    # subset df to get only I and covariates (traits)
+    df_interact <- df_interact[, -c(1:2)]
+
+    # guess and cast each columns in the right data types
+    df_interact <- data.frame(I = df_interact[, 1], as.data.frame(sapply(df_interact[, -c(1)],
+        type.convert)))
 
     if (family == "randomForest") {
-        model <- randomForest::randomForest(I ~ . * ., intertrait, ...)
+        model <- randomForest::randomForest(formula, df_interact, ...)
     } else {
-        model <- stats::glm(I ~ . * ., intertrait, family = family, ...)
+        model <- stats::glm(formula, df_interact, family = family, ...)
+        if (step == TRUE){
+          model <- step(model)
+        }
     }
 
 
