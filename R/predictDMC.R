1f91e0aa1bbf5c4dc8bef5e8db8d6fb95d05a237
Steve Vissault <s.vissault@yahoo.fr>
2017-05-24 09:35:04 -0400

    push propriété des noeuds

---
+++ R/predictDMC.R
@@ -0,0 +1,44 @@
+#' @name predictDMC
+#'
+#' @title prediction based on direct matching centrality method
+#'
+#' @description predict species interaction based on the direct matching centrality model
+#'
+#' @param model
+#' @param newdata
+#' @author
+#' Dominique Gravel & Steve Vissault
+#'
+#'
+#' @export
+predictDMC <- function(model, data = NULL, type = "response") {
+
+    if (model$level == "species") {
+
+        data <- bartomeus
+
+        interactSp <- reshape2::melt(data$interactSp, na.rm = TRUE)
+        names(interactSp) <- c("idFrom", "idTo", "n_interact")
+
+        traitSp <- reshape2::dcast(data$traitSp, idSp ~ traitName)
+        traitSp <- data.frame(idSp = traitSp[, 1], as.data.frame(sapply(traitSp[,
+            -c(1)], type.convert)))
+
+
+        # Cast trait TO
+        interactSp <- merge(interactSp, traitSp, by.x = "idFrom", by.y = "idSp",
+            all.x = TRUE)
+
+        # Cast trait FROM
+        interactSp <- merge(interactSp, traitSp, by.x = "idTo", by.y = "idSp",
+            all.x = TRUE)
+
+
+
+    }
+
+
+
+
+
+}
