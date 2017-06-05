786821313945cd5e3997503463881640eda3e566
Kevin Cazelles <kevin.cazelles@um2.fr>
2017-06-04 21:25:53 -0400

    review as.alienData

--- tests/testthat/test-alienData.R
+++ tests/testthat/test-alienData.R
@@ -1,40 +1,62 @@
 context("alienData function")
 
+nind <- 10
+df_ex1 <- data.frame(
+  idInd = paste0("ind", sprintf("%02d", 1:10)),
+  idSp = paste0("sp", sprintf("%02d", 1:10)),
+  stringsAsFactors=FALSE)
+df_ex4 <- df_ex3 <- df_ex2 <- df_ex1
+df_ex2$idInd[nind] <- df_ex1$idInd[1L]
+df_ex3$idSp[nind] <- df_ex1$idSp[1L]
+df_ex4$idInd[nind] <- df_ex1$idSp[1L]
+
+##
+sp1 <- sample(1:10, 8, replace=T)
+sp2 <- sample(1:10, 8, replace=T)
+##
+df_int <- cbind(idFrom = paste0("sp", sprintf("%02d", sp1)), idTo = paste0("sp", sprintf("%02d", sp2)))
+
+
 test_that("check data structure", {
-
-  load('test_idObs.rda')
-  out <- as.alienData(idObs=idObs, interactPair=interactPair, verbose=FALSE)
-
-  expect_is(out, "alienData")
-  # Even if items from the list are NULL, all items have to be returned
-  expect_equal(names(out), c("idObs", "interactPair", "interactSp",
-    "interactInd", "coOcc", "coAbund", "siteEnv", "traitSp", "traitInd", "phy"))
+  expect_error(as.alienData(df_ex2, df_int))
+  expect_error(as.alienData(df_ex3[,-1], df_int))
+  expect_error(as.alienData(df_ex4, df_int))
+  expect_equal(as.alienData(df_ex1)$nbSpecies, 10)
 })
-
-test_that("check data integrity", {
-
-  # load fake data
-  load('test_idObs.rda')
-
-  interactPair <-  data.frame(idTo=c("1","sp1"),idFrom=c("1","2"),strength=c(NA,NA), stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"'idTo' values belongs to 'idSp' and 'idInd' in 'idObs'. Interaction can't be at the species AND individual levels")
-
-  interactPair <-  data.frame(idTo=c("1","2"),idFrom=c("sp1","2"),strength=c(NA,NA), stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"'idFrom' values belongs to 'idSp' and 'idInd' in 'idObs'. Interaction can't be at the species AND individual levels")
-
-  interactPair <- data.frame(idTo="sp4",idFrom="sp1",strength=NA,stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some species ids in 'idTo' are not in 'idObs'")
-
-  interactPair <-  data.frame(idTo="4",idFrom="1",strength=NA,stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some individus ids in 'idTo' are not in 'idObs'")
-
-  interactPair <- data.frame(idTo="sp1",idFrom="sp4",strength=NA,stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some species ids in 'idFrom' are not in 'idObs'")
-
-  interactPair <-  data.frame(idTo="1",idFrom="4",strength=NA,stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some individus ids in 'idFrom' are not in 'idObs'")
-
-  interactPair <-  data.frame(idTo=c("2","2","3"),idFrom=c("2","2","2"),strength=c(NA,NA,NA), stringsAsFactors=FALSE)
-  expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some 'idFrom' and 'idTo' are duplicated")
-
-})
+#
+#   load('test_idObs.rda')
+#   out <- as.alienData(idObs=idObs, interactPair=interactPair, verbose=FALSE)
+#
+#   expect_is(out, "alienData")
+#   # Even if items from the list are NULL, all items have to be returned
+#   expect_equal(names(out), c("idObs", "interactPair", "interactSp",
+#     "interactInd", "coOcc", "coAbund", "siteEnv", "traitSp", "traitInd", "phy"))
+# })
+#
+# test_that("check data integrity", {
+#
+#   # load fake data
+#   load('test_idObs.rda')
+#
+#   interactPair <-  data.frame(idTo=c("1","sp1"),idFrom=c("1","2"),strength=c(NA,NA), stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"'idTo' values belongs to 'idSp' and 'idInd' in 'idObs'. Interaction can't be at the species AND individual levels")
+#
+#   interactPair <-  data.frame(idTo=c("1","2"),idFrom=c("sp1","2"),strength=c(NA,NA), stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"'idFrom' values belongs to 'idSp' and 'idInd' in 'idObs'. Interaction can't be at the species AND individual levels")
+#
+#   interactPair <- data.frame(idTo="sp4",idFrom="sp1",strength=NA,stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some species ids in 'idTo' are not in 'idObs'")
+#
+#   interactPair <-  data.frame(idTo="4",idFrom="1",strength=NA,stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some individus ids in 'idTo' are not in 'idObs'")
+#
+#   interactPair <- data.frame(idTo="sp1",idFrom="sp4",strength=NA,stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some species ids in 'idFrom' are not in 'idObs'")
+#
+#   interactPair <-  data.frame(idTo="1",idFrom="4",strength=NA,stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some individus ids in 'idFrom' are not in 'idObs'")
+#
+#   interactPair <-  data.frame(idTo=c("2","2","3"),idFrom=c("2","2","2"),strength=c(NA,NA,NA), stringsAsFactors=FALSE)
+#   expect_error(as.alienData(idObs=idObs, interactPair=interactPair,verbose=FALSE),"Some 'idFrom' and 'idTo' are duplicated")
+#
+# })
