c156b14413177cc9c5da6b64361a151e9a925719
Kevin Cazelles <kevin.cazelles@um2.fr>
2017-06-07 00:10:54 -0400

    as.alienData function reviewed v2

--- R/as.alienData.R
+++ R/as.alienData.R
@@ -2,39 +2,39 @@
 #'
 #' @description This function formats the data and returns an object of class alienData.
 #'
-#' @param dfSpecies A data frame with at least one column named \code{idSp} providing
-#' unique identifiers for each species identified within the dataset. Optionnaly,
-#' a \code{idSp} column could be specied and will be detected as information on
-#' individuals. The remainig columns could be either traits or phylogenetic or taxonomic
-#' data that must be specified respectively by \code{trait}, \code{phylo} or \code{taxo}
+#' @param dfNodes A data frame with at least one column named \code{idNodes} providing
+#' unique identifiers for each species identified within the dataset. The remainig
+#' columns could be either traits or phylogenetic or taxonomic data that must
+#' be specified respectively by \code{trait}, \code{phylo} or \code{taxo}
 #' parameter described below.
-#' @param dfInteract A data frame with at least two columns: \code{idFrom}  which contains interaction at the finest level
-#' (individus or species). The first two columns are \code{idFrom} and \code{idTo} and
-#' determine the sens of the interaction. idFrom and \code{idTo} are unique identifier
-#' of species or individu documented in the \code{idObs} data frame. Finaly, the thrid
-#' column is the strength of the interaction (Please see details).
-#' @param trait A square symmetric matrix of 0s and 1s that define co-occurence patterns among pairs of species. If this matrix is not provided, the co-occurence matrix is derived from the coAbund matrix else the \code{idObs} dataframe (see return section).
-#' @param phylo A square symmetric matrix that includes any types of values, defining co-abundance patterns among pairs of species. TODO: Not implemented yet.
-#' @param taxo A matrix or a data frame where each column is a descriptor of the sites. TODO: siteEnv should cover the possibility that environmental variables could be taken at several times - link to idTime in idObs?.
-#' @param binary Logical. Should the
-#' @param directed Logical. (see \code{Details}).
+#' @param dfEdges A data frame with at least two columns: \code{idFrom} and \code{idTo}
+#' descibring the set of edges. If \code{directed} is set to \code{TRUE} is also
+#' describes the direction of the interaction. Two additonnal columns are checked
+#' \code{value} \code{iDsite} which respectively stand for the value associated
+#' with the edge and where the interaction has been observed.
+#' @param trait A vector indicating columns number (or names) of \code{dfNodes} containing traits data (see \code{Details}).
+#' @param phylo A vector indicating colums number (or names) of \code{dfNodes} containing phylo data (see \code{Details}).
+#' @param taxo A vector indicating columns number (or names) of \code{dfNodes} containing taxo data (see \code{Details}).
+#' @param binary Logical. If `TRUE`, interactions are turned into a boolean variable (see \code{Details}).
+#' @param directed Logical. If `TRUE` (default value) the network is considered as directed (see \code{Details}).
 #' @param dfSite A data frame with at least two columns named \code{idFrom}
-#' @param dfOcc Logical
-#' @param verbose Logical
+#' @param dfOcc A data frame with at least two columns
+#' @param verbose Logical. Should extra information be reported on progress?
 #'
 #' @details
 #'
-#' The user is required to provide specific column names to prevent from generating errors.
+#' The user is required to provide specific column names to prevent from
+#' generating errors. Two primary keys \code{idNodes} and \code{idSite} if
+#' argument \code{dfSite} is not null.
 #'
 #' trait/phylo/taxo non exlusive a column be three or both. There are associated and help distinguishing methods.
 #'
 #' character of factors => as.charcater
 #' The strength of the interactions defined in the third column of
-#' \code{interactPair} can be a 0 if no direct interaction has been observed
-#' (defined as true absence of interaction) or any numerical value.
-#' WARNING: interactSp is aggregating the information at the species level using the sum function by default.
-#' Undocumented interactions among species or individus will be assumed as NA
-#' by default.
+#'
+#' If no values in \code{dfEdges} then values are set to 1 is used.
+#' \code{directed} => methods and => plots
+#' \code{binary} => methods and option
 #'
 #' @return
 #' An object of the class \code{alienData} is returned by \code{as.alienData}.
@@ -50,50 +50,37 @@
 #' @export
 
 
-as.alienData <- function(dfSpecies, dfInteract, trait = NULL, phylo = NULL, taxo = NULL, 
+as.alienData <- function(dfNodes, dfEdges, trait = NULL, phylo = NULL, taxo = NULL, 
     siteEnv = NULL, traitSp = NULL, traitInd = NULL, phy = NULL, dfSite = NULL, dfOcc = NULL, 
     binary = FALSE, directed = FALSE, verbose = TRUE) {
     
-    ############################## dfSpecies
-    dfSpecies %<>% as.data.frame(stringsAsFactors = FALSE)
-    dfInteract %<>% as.data.frame(stringsAsFactors = FALSE)
+    ############################## dfNodes
+    dfNodes %<>% as.data.frame(stringsAsFactors = FALSE)
+    dfEdges %<>% as.data.frame(stringsAsFactors = FALSE)
     ## 
-    stopifnot("idSp" %in% names(dfSpecies))
-    stopifnot("idFrom" %in% names(dfInteract))
-    stopifnot("idTo" %in% names(dfInteract))
+    stopifnot("idNodes" %in% names(dfNodes))
+    stopifnot("idFrom" %in% names(dfEdges))
+    stopifnot("idTo" %in% names(dfEdges))
     ## 
-    dfSpecies$idSp %<>% as.character
-    dfInteract$idFrom %<>% as.character
-    dfInteract$idTo %<>% as.character
+    dfNodes$idNodes %<>% as.character
+    dfEdges$idFrom %<>% as.character
+    dfEdges$idTo %<>% as.character
     ## 
-    dfMethAvail <- data.frame(methods = c("Co-occurence", "Direct Matching Centrality", 
+    availableMeths <- data.frame(methods = c("Co-occurence", "Direct Matching Centrality", 
         "iEat"), available = FALSE, stringsAsFactors = FALSE)
+    
     ## 
-    if ("idInd" %in% names(dfSpecies)) {
-        indiv <- TRUE
-        # dfInteract$idInd %<>% as.character()
-        stopifnot(!any(table(dfSpecies$idInd) > 1))
-        stopifnot(!any(dfSpecies$idInd %in% dfSpecies$idSp))
-        nbIndividuals <- nrow(dfSpecies)
-        if (verbose) 
-            message("==> Information at the individual level detected")
-    } else {
-        if (verbose) {
-            nbIndividuals <- NULL
-            message("==> Information at the species level detected")
-            stopifnot(!any(table(dfSpecies$idSp) > 1))
-        }
-    }
-    nbSpecies <- dfSpecies$idSp %>% unique %>% length
+    stopifnot(!any(table(dfNodes$idNodes) > 1))
+    message("==> Nodes information detected")
     
     ## 
     sc <- 0
     if (is.null(trait)) {
         nmTrait <- NULL
         if (verbose) 
-            message("==> No trait detected")
+            message("==> No traits detected")
     } else {
-        nmTrait <- names(dfSpecies[, trait])
+        nmTrait <- names(dfNodes[, trait])
         sc <- 1
         if (verbose) 
             message(paste0("==> Traits detected: ", paste(nmTrait, collapse = ", ")))
@@ -102,9 +89,9 @@ as.alienData <- function(dfSpecies, dfInteract, trait = NULL, phylo = NULL, taxo
     if (is.null(phylo)) {
         nmPhylo <- NULL
         if (verbose) 
-            message("==> No phylogeny detected")
+            message("==> No phylo detected")
     } else {
-        nmPhylo <- names(dfSpecies[, phylo])
+        nmPhylo <- names(dfNodes[, phylo])
         sc <- 1
         if (verbose) 
             message(paste0("==> Phylo detected: ", paste(nmPhylo, collapse = ", ")))
@@ -113,61 +100,40 @@ as.alienData <- function(dfSpecies, dfInteract, trait = NULL, phylo = NULL, taxo
     if (is.null(taxo)) {
         nmTaxo <- NULL
         if (verbose) 
-            message("==> No taxonomy detected")
+            message("==> No taxon detected")
     } else {
-        nmTaxo <- names(dfSpecies[, taxo])
+        nmTaxo <- names(dfNodes[, taxo])
         sc <- 1
         if (verbose) 
-            message(paste0("==> Taxonomy detected: ", paste(nmTaxo, collapse = ", ")))
+            message(paste0("==> Taxo detected: ", paste(nmTaxo, collapse = ", ")))
     }
     ## 
     if (sc) {
-        dfMethAvail$available[dfMethAvail$methods == "iEat"] <- TRUE
-        dfMethAvail$available[dfMethAvail$methods == "Direct Matching Centrality"] <- TRUE
+        availableMeths$available[availableMeths$methods == "iEat"] <- TRUE
+        availableMeths$available[availableMeths$methods == "Direct Matching Centrality"] <- TRUE
     }
     
     
-    ############################## dfInteract
-    indint <- FALSE  # Interactions described at the individual level?
-    if (indiv) {
-        if (all(dfInteract$idFrom %in% dfSpecies$idInd)) {
-            stopifnot(all(dfInteract$idTo %in% dfSpecies$idInd))
-            indint <- TRUE
-            if (verbose) 
-                message("==> Interactions described at the individual level")
-            
-        } else {
-            message("==> Trying to find interactions at the species level")
-            stopifnot(all(dfInteract$idFrom %in% dfSpecies$idSp))
-            stopifnot(all(dfInteract$idTo %in% dfSpecies$idSp))
-            if (verbose) 
-                message("==> Interactions described at the species level")
-        }
-    } else {
-        stopifnot(all(dfInteract$idFrom %in% dfSpecies$idSp))
-        stopifnot(all(dfInteract$idTo %in% dfSpecies$idSp))
+    ############################## dfEdges
+    stopifnot(all(dfEdges$idFrom %in% dfNodes$idNodes))
+    stopifnot(all(dfEdges$idTo %in% dfNodes$idNodes))
+    
+    if (!"value" %in% names(dfEdges)) {
         if (verbose) 
-            message("==> Interactions described at the species level")
-    }
-    ## 
-    if (!"value" %in% names(dfInteract)) 
-        dfInteract$value <- 1
-    if (binary) {
-        dfInteract$value <- dfInteract$value > 0
-    }
-    ## 
-    if (indint) {
-        interLevel <- "individuals"
-    } else interLevel <- "species"
-    ### 
-    nbInteractions <- nrow(dfInteract)
+            message("==> No values associated with edges, values are set to 1")
+        dfEdges$value <- 1
+    } else message("==> Edges values detected")
     
+    if (binary) {
+        dfEdges$value <- dfEdges$value > 0
+        message("==> Interactions are considered as binary")
+    }
     
     
     ############################## dfSite
     if (is.null(dfSite)) {
         if (verbose) 
-            message("==> No site information detected")
+            message("==> No site info detected")
         nbSites <- NULL
     } else {
         stopifnot("idSite" %in% names(dfSite))
@@ -175,55 +141,45 @@ as.alienData <- function(dfSpecies, dfInteract, trait = NULL, phylo = NULL, taxo
         dfSite$idSite %<>% as.character
         nmSite <- names(dfSite)
         if (verbose) 
-            message(paste0("==> Site information detected: ", paste(nmSite, collapse = ", ")))
-        if ("idSite" %in% names(dfInteract)) {
-            stopifnot(all(dfSite$idSite %in% dfInteract$idSite))
+            message(paste0("==> Site info detected: ", paste(nmSite, collapse = ", ")))
+        if ("idSite" %in% names(dfEdges)) {
+            stopifnot(all(dfSite$idSite %in% dfEdges$idSite))
         }
         nbSites <- nrow(dfSite)
     }
     
     
-    
     ############################## dfOcc
     occ <- FALSE
-    if ("idSite" %in% names(dfInteract) & is.null(dfOcc)) {
-        stopifnot(all(dfInteract$idSite %in% dfSite$idSite))
-        dfInteract$idSite %<>% as.character
+    if ("idSite" %in% names(dfEdges) & is.null(dfOcc)) {
+        stopifnot(all(dfEdges$idSite %in% dfSite$idSite))
+        dfEdges$idSite %<>% as.character
         if (verbose) 
-            message("==> Trying to get occurrence information from 'dfInteract'...")
+            message("==> Getting occurrence information from 'dfEdges'...")
         ## 
-        dfOcc <- data.frame(id = c(dfInteract$idTo, dfInteract$idFrom), idSite = rep(dfInteract$idSite, 
+        dfOcc <- data.frame(id = c(dfEdges$idTo, dfEdges$idFrom), idSite = rep(dfEdges$idSite, 
             2), stringsAsFactors = FALSE) %>% unique
         ## 
-        if (indint) 
-            names(dfOcc)[1L] <- "idInd" else names(dfOcc)[1L] <- "idSp"
+        names(dfOcc)[1L] <- "idNodes"
     }
+    
     
     if (!is.null(dfOcc)) {
         stopifnot("idSite" %in% names(dfOcc))
         dfOcc$idSite %<>% as.character
-        stopifnot("idSp" %in% names(dfOcc) | "idInd" %in% names(dfOcc))
-        stopifnot(all(dfOcc$idSite %in% dfSite$idSite))
+        stopifnot("idNodes" %in% names(dfOcc))
         ## 
-        if ("idInd" %in% names(dfOcc)) {
-            stopifnot(all(dfOcc$idInd %in% dfSpecies$idInd))
-            occ <- TRUE
-            if (!all(dfSpecies$idInd %in% dfOcc$idInd)) 
-                warning("Individuals without any occurrence record.")
-            dfOcc$idInd %<>% as.character
-            if (verbose) 
-                message("==> Occurrence information detected at the individuals level")
-        } else {
-            stopifnot(all(dfOcc$idSp %in% dfSpecies$idSp))
-            occ <- TRUE
-            if (!all(dfSpecies$idSp %in% dfOcc$idSp)) 
-                warning("Species without any occurrence record.")
-            if (verbose) 
-                message("==> Occurrence information detected at the species level")
-            dfOcc$idSp %<>% as.character
-        }
+        stopifnot(all(dfOcc$idSite %in% dfSite$idSite))
+        stopifnot(all(dfOcc$idNodes %in% dfNodes$idNodes))
+        occ <- TRUE
+        if (!all(dfNodes$idNodes %in% dfOcc$idNodes)) 
+            warning("Nodes without any occurrence record.")
+        if (verbose) 
+            message("==> Occurrence information detected")
+        dfOcc$idNodes %<>% as.character
+        ## 
         nbOcc <- nrow(dfOcc)
-        dfMethAvail$available[dfMethAvail$methods == "Co-occurence"] <- TRUE
+        availableMeths$available[availableMeths$methods == "Co-occurence"] <- TRUE
     } else {
         if (nbSites > 0) 
             warning("Site information provided without any occurrence")
@@ -232,10 +188,10 @@ as.alienData <- function(dfSpecies, dfInteract, trait = NULL, phylo = NULL, taxo
     
     
     ############################## Return results
-    res <- list(nbSpecies = nbSpecies, nbIndividuals = nbIndividuals, nbInteractions = nbInteractions, 
-        interLevel = interLevel, directed = directed, nbSites = nbSites, nbOcc = nbOcc, 
-        dfSpecies = dfSpecies, dfInteract = dfInteract, dfSite = dfSite, dfOcc = dfOcc, 
-        nmTrait = nmTrait, nmPhylo = nmPhylo, nmTaxo = nmTaxo, nmSite = nmSite, dfMethAvail = dfMethAvail)
+    res <- list(nbNodes = nrow(dfNodes), nbInteractions = nrow(dfEdges), directed = directed, 
+        nbSites = nbSites, nbOcc = nbOcc, dfNodes = dfNodes, dfEdges = dfEdges, dfSite = dfSite, 
+        dfOcc = dfOcc, nmTrait = nmTrait, nmPhylo = nmPhylo, nmTaxo = nmTaxo, nmSite = nmSite, 
+        availableMeths = availableMeths)
     
     class(res) <- "alienData"
     return(res)
--- tests/testthat/test-alienData.R
+++ tests/testthat/test-alienData.R
@@ -7,15 +7,15 @@ nsit <- 10
 nint <- 15
 ##
 df_ex0 <- data.frame(
-  idInd = paste0("ind", sprintf("%02d", 1:nind)),
+  idNodes = paste0("ind", sprintf("%02d", 1:nind)),
   idSp = paste0("sp", sprintf("%02d", sample(1:nsp, nind, replace=TRUE))),
   stringsAsFactors=FALSE)
 ##
 df_exa <- df_ex3 <- df_ex2 <- df_ex1 <- df_ex0
 names(df_exa)[2] <- "val1"
-df_ex1$idInd[nind] <- df_ex1$idInd[1L]
+df_ex1$idNodes[nind] <- df_ex1$idNodes[1L]
 df_ex2$idSp[nind] <- df_ex1$idSp[1L]
-df_ex3$idInd[nind] <- df_ex1$idSp[1L]
+df_ex3$idNodes[nind] <- df_ex1$idSp[1L]
 df_ex4 <- cbind(df_ex1, val1 = runif(nind))
 
 ####
@@ -52,7 +52,7 @@ df_int6$idSite[1L] <- df_int6$idSite[nint]
 ###
 df_occ0 <-  data.frame(
   idSite = paste0("site", sprintf( "%02d", sample(1:nsit, 2*nind, replace=TRUE))),
-  idInd = rep(df_ex0$idInd, 2),
+  idNodes = rep(df_ex0$idNodes, 2),
   stringsAsFactors = FALSE)
 df_occ4 <-df_occ3 <- df_occ2 <- df_occ1 <- df_occ0
 names(df_occ1)[1L] <- "new"
@@ -64,47 +64,47 @@ df_occ4[1L,2L] <- "new"
 res0 <- as.alienData(df_ex0, df_int0, dfSite=df_sit0, verbose=F)
 res1 <- as.alienData(df_ex0, df_int0, dfSite=df_sit0, dfOcc = df_occ0, verbose=F)
 
-
-test_that("check dfSpecies", {
-  expect_error(as.alienData(df_exa, df_int0), '"idSp" %in% names(dfSpecies) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex1, df_int0), "!any(table(dfSpecies$idInd) > 1) is not TRUE", fixed = TRUE)
-  expect_error(as.alienData(df_ex2[,-1], df_int0), '"idSp" %in% names(dfSpecies) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex3, df_int0), "!any(dfSpecies$idInd %in% dfSpecies$idSp) is not TRUE", fixed = TRUE)
-  expect_error(as.alienData(df_ex4, df_int0, trait=4))
-  expect_error(as.alienData(df_ex4, df_int0, phylo="val2"))
-  expect_error(as.alienData(df_ex4, df_int0, taxo=4))
-})
-
-
-test_that("check dfInteract", {
-  expect_error(as.alienData(df_ex0, df_inta), '"idFrom" %in% names(dfInteract) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_intb), '"idTo" %in% names(dfInteract) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_int1), "all(dfInteract$idFrom %in% dfSpecies$idSp) is not TRUE", fixed=TRUE)
-  expect_error(as.alienData(df_ex0, df_int2), "all(dfInteract$idTo %in% dfSpecies$idInd) is not TRUE", fixed=TRUE)
-})
-
-test_that("check dfSite", {
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit1), '"idSite" %in% names(dfSite) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit2), "all(table(dfSite$idSite) == 1) is not TRUE", fixed = TRUE)
-})
-
-test_that("check dfOcc", {
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ1), '"idSite" %in% names(dfOcc) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ2), 'idSp" %in% names(dfOcc) | "idInd" %in% names(dfOcc) is not TRUE', fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ3), "all(dfOcc$idSite %in% dfSite$idSite) is not TRUE", fixed = TRUE)
-  expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ4), "all(dfOcc$idInd %in% dfSpecies$idInd) is not TRUE", fixed = TRUE)
-})
-
-
-test_that("check output values", {
-  expect_warning(as.alienData(df_ex0, df_int0, dfSite=df_sit0, verbose=F), "Site information provided without any occurrence")
-  expect_is(res1, "alienData")
-  expect_equal(res1$nbIndividuals, nind)
-  expect_equal(res1$nbSpecies, nsp)
-  expect_equal(res1$nbInteractions, nint)
-  expect_equal(res1$nbSite, nsit)
-  expect_equal(res1$dfMethAvail$available, c( TRUE, FALSE, FALSE))
-})
+#
+# test_that("check dfSpecies", {
+#   expect_error(as.alienData(df_exa, df_int0), '"idSp" %in% names(dfSpecies) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex1, df_int0), "!any(table(dfSpecies$idNodes) > 1) is not TRUE", fixed = TRUE)
+#   expect_error(as.alienData(df_ex2[,-1], df_int0), '"idSp" %in% names(dfSpecies) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex3, df_int0), "!any(dfSpecies$idNodes %in% dfSpecies$idSp) is not TRUE", fixed = TRUE)
+#   expect_error(as.alienData(df_ex4, df_int0, trait=4))
+#   expect_error(as.alienData(df_ex4, df_int0, phylo="val2"))
+#   expect_error(as.alienData(df_ex4, df_int0, taxo=4))
+# })
+#
+#
+# test_that("check dfInteract", {
+#   expect_error(as.alienData(df_ex0, df_inta), '"idFrom" %in% names(dfInteract) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_intb), '"idTo" %in% names(dfInteract) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_int1), "all(dfInteract$idFrom %in% dfSpecies$idSp) is not TRUE", fixed=TRUE)
+#   expect_error(as.alienData(df_ex0, df_int2), "all(dfInteract$idTo %in% dfSpecies$idNodes) is not TRUE", fixed=TRUE)
+# })
+#
+# test_that("check dfSite", {
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit1), '"idSite" %in% names(dfSite) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit2), "all(table(dfSite$idSite) == 1) is not TRUE", fixed = TRUE)
+# })
+#
+# test_that("check dfOcc", {
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ1), '"idSite" %in% names(dfOcc) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ2), 'idSp" %in% names(dfOcc) | "idNodes" %in% names(dfOcc) is not TRUE', fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ3), "all(dfOcc$idSite %in% dfSite$idSite) is not TRUE", fixed = TRUE)
+#   expect_error(as.alienData(df_ex0, df_int0, dfSite = df_sit0, dfOcc = df_occ4), "all(dfOcc$idNodes %in% dfSpecies$idNodes) is not TRUE", fixed = TRUE)
+# })
+#
+#
+# test_that("check output values", {
+#   expect_warning(as.alienData(df_ex0, df_int0, dfSite=df_sit0, verbose=F), "Site information provided without any occurrence")
+#   expect_is(res1, "alienData")
+#   expect_equal(res1$nbIndividuals, nind)
+#   expect_equal(res1$nbSpecies, nsp)
+#   expect_equal(res1$nbInteractions, nint)
+#   expect_equal(res1$nbSite, nsit)
+#   expect_equal(res1$dfMethAvail$available, c( TRUE, FALSE, FALSE))
+# })
 
 # TO DO
 # A robust mechanism to determine the level of information
