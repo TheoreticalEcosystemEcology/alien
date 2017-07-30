81e804baf65e0a495c397f229c675454d1ebbeec
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-07-29 20:47:22 -0400

    simplify the parameters' count in fitMC

--- tests/testthat/test-fitMC.R
+++ tests/testthat/test-fitMC.R
@@ -5,5 +5,9 @@ source("minimalEx.R")
 ald1 <- alienData(df_nd0, df_int0, dfSites=df_sit0, dfOcc = df_occ0, verbose=F)
 # the line below take a while so I commented for now (I'll change mxt and
 # implement few tets soon).
-# fitMC(ald1, d=5, 10)
-# error fitMC(ald1, d=6, 10)
+# fitMC(ald1, d=1, 10)
+
+
+test_that("check overfit", {
+  expect_error(fitMC(ald1, d=7, 10), 'npar < prod(dim(netObs)) is not TRUE', fixed = TRUE)
+})
