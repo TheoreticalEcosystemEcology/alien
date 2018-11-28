c84f72dfd8b0b2345a0d62265aad543e55e4501c
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-11-28 12:00:16 -0500

    More work on alienData

--- R/alienData.R
+++ R/alienData.R
@@ -3,7 +3,7 @@
 #' @description \code{alienData} is used to check and format data, if correct
 #' it returns an object of class \code{alienData}.
 #'
-#' @param nodes A vector, data.frame or list of data.frame with at least one column named \code{idNodes} providing
+#' @param nodes A data.frame with two columns. The first column includes unique individual identifications and the second columns presents species identification. (See details).
 #' unique identifiers for each species considered. The remaining
 #' columns are traits data that must
 #' be specified in the \code{trait} argument, otherwise they will be ignored.
@@ -27,7 +27,7 @@
 #'
 #' @details
 #'
-#' In the \code{nodes} argument, each layer of the list needs to include information about a particular layer of species in the network. If all species can potentially interact among each other, only a vector or a data.frame can be included. 
+#' In the \code{nodes} argument, the first columns (individual identification) identifathe first columns should have noeach layer of the list needs to include information about a particular layer of species in the network. If all species can potentially interact among each other, only a vector or a data.frame can be included. 
 #'
 #' The user is required to provide specific column names to prevent the function
 #' from returning errors. Two primary keys \code{idNodes} and \code{idSite} (if site
@@ -73,194 +73,5 @@ nodes <- list(salix = salix, galler = galler, paras = paras)
 
 alienData <- function(nodes, dfEdges, trait = NULL, phylo = NULL, taxo = NULL,
     dfSites = NULL, siteEnv = NULL, dfOcc = NULL, directed = FALSE, verbose = TRUE) {
-
-    #### handle options
-    osaf <- options()
-    options(stringsAsFactors = FALSE)
-    on.exit(options(osaf))
-
-    #### nodes 
-    # Vector
-    if (is.atomic(nodes)) {
-      nodes <- list(df1 = data.frame(ID = as.character(nodes)))
-    }
-    
-    # data.frame
-    if (is.data.frame(nodes)) {
-      nodes <- list(df1 = nodes)
-    }
-   
-    # List
-    if(is.list(nodes)){
-      checkDf <- sapply(nodes, is.data.frame)
-      if(!all(checkdf)){
-        nodesNoDf <- which(!checkdf)
-        for(i in nodesNoDf){
-          if(is.vector(nodes[[i]])){
-            nodes[[i]] <- data.frame(idNodes = as.character(nodes[[i]]))
-          }
-        }
-      }
-    }
-    
-    ### ICI ###
-    ### ICI ###
-    ### ICI ###
-    ##
-    dfEdges %<>% as.data.frame
-    
-    ##
-    stopifnot("idNodes" %in% names(nodes))
-    stopifnot("idFrom" %in% names(dfEdges))
-    stopifnot("idTo" %in% names(dfEdges))
-    
-    ##
-    nodes$idNodes %<>% as.character
-    dfEdges$idFrom %<>% as.character
-    dfEdges$idTo %<>% as.character
-    
-    ##
-    availableMeths <- data.frame(methods = c("Co-occurence", "Direct Matching Centrality",
-        "iEat"), available = FALSE)
-
-    ##
-    stopifnot(!any(table(nodes$idNodes) > 1))
-    if (verbose)
-        message("==> Nodes information detected")
-
-    ##
-    sc <- 0
-    if (is.null(trait)) {
-        nmTrait <- NULL
-        if (verbose)
-            message("==> No traits detected")
-    } else {
-        nmTrait <- names(nodes[, trait, drop = FALSE])
-        sc <- 1
-        if (verbose)
-            message(paste0("==> Traits detected: ", paste(nmTrait, collapse = ", ")))
-    }
-    ##
-    if (is.null(phylo)) {
-        nmPhylo <- NULL
-        if (verbose)
-            message("==> No phylo detected")
-    } else {
-        nmPhylo <- names(nodes[, phylo, drop = FALSE])
-        sc <- 1
-        if (verbose)
-            message(paste0("==> Phylo detected: ", paste(nmPhylo, collapse = ", ")))
-    }
-    ##
-    if (sc) {
-        availableMeths$available[availableMeths$methods == "iEat"] <- TRUE
-        availableMeths$available[availableMeths$methods == "Direct Matching Centrality"] <- TRUE
-    }
-
-
-    ############################## dfEdges
-    stopifnot(all(dfEdges$idFrom %in% nodes$idNodes))
-    stopifnot(all(dfEdges$idTo %in% nodes$idNodes))
-    idn <- which(!nodes$idNodes %in% c(dfEdges$idFrom, dfEdges$idTo))
-    if (length(idn))
-        warning(paste0("Unlinked nodes: ", paste(nodes$idNodes[idn], collapse = ", ")))
-
-    if (!"value" %in% names(dfEdges)) {
-        if (verbose)
-            message("==> No Edges' value detected, values are set to 1")
-        dfEdges$value <- 1
-    } else if (verbose)
-        message("==> Edges' values detected")
-
-
-    #### dfSites
-    nmSite <- NULL
-    if (is.null(dfSites)) {
-        if ("idSite" %in% names(dfEdges)) {
-            if (verbose)
-                message("==> Sites' ID are provided by dfEdges")
-            dfSites <- data.frame(idSite = unique(dfEdges$idSite))
-            nbSites <- nrow(dfSites)
-        } else {
-            if (verbose)
-                message("==> No site info detected")
-            nbSites <- NULL
-        }
-
-    } else {
-        stopifnot("idSite" %in% names(dfSites))
-        stopifnot(all(table(dfSites$idSite) == 1))
-        ##
-        if (!is.null(siteEnv)) {
-            ##
-            dfSites <- cbind(idSite = dfSites$idSite, dfSites[, siteEnv, drop = FALSE])
-            dfSites <- dfSites[, unique(names(dfSites))]
-            ##
-        }
-        if (ncol(dfSites) > 1) {
-            nmSite <- names(dfSites)[-1L]
-            if (verbose)
-                message(paste0("==> Site info detected: ", paste(nmSite, collapse = ", ")))
-        } else if (verbose)
-            message("==> No site info detected")
-        ##
-        dfSites$idSite %<>% as.character
-        if ("idSite" %in% names(dfEdges)) {
-            if (!all(dfSites$idSite %in% dfEdges$idSite)) {
-                warnings("Sites without interaction records")
-            }
-        }
-        nbSites <- nrow(dfSites)
-    }
-
-    #### dfOcc
-    occ <- FALSE
-    if ("idSite" %in% names(dfEdges) & is.null(dfOcc)) {
-        stopifnot(all(dfEdges$idSite %in% dfSites$idSite))
-        dfEdges$idSite %<>% as.character
-        if (verbose)
-            message("==> Getting occurrence information from 'dfEdges'...")
-        ##
-        dfOcc <- data.frame(id = c(dfEdges$idTo, dfEdges$idFrom), idSite = rep(dfEdges$idSite,
-            2)) %>% unique
-        ##
-        names(dfOcc)[1L] <- "idNodes"
-    }
-
-
-    if (!is.null(dfOcc)) {
-        stopifnot("idSite" %in% names(dfOcc))
-        dfOcc$idSite %<>% as.character
-        stopifnot("idNodes" %in% names(dfOcc))
-        ##
-        stopifnot(all(dfOcc$idSite %in% dfSites$idSite))
-        stopifnot(all(dfOcc$idNodes %in% nodes$idNodes))
-        occ <- TRUE
-        if (!all(nodes$idNodes %in% dfOcc$idNodes))
-            warning("Nodes without any occurrence record.")
-        if (verbose)
-            message("==> Occurrence information detected")
-        dfOcc$idNodes %<>% as.character
-        ##
-        nbOcc <- nrow(dfOcc)
-        availableMeths$available[availableMeths$methods == "Co-occurence"] <- TRUE
-    } else {
-        if (!is.null(nbSites))
-            warning("Site information provided without any occurrence")
-        nbOcc <- NULL
-    }
-
-
-    #### Return results
-    res <- list(nodes = nodes, dfEdges = dfEdges, dfSites = dfSites,
-                dfOcc = dfOcc, info = list(nbNodes = nrow(nodes),
-                                           nbEdges = nrow(dfEdges),
-                                           directed = directed, 
-                                           nbSites = nbSites, 
-                                           nbOcc = nbOcc, 
-                                           nmTrait = nmTrait, 
-                                           nmSite = nmSite, 
-                                           availableMeths = availableMeths))
-    class(res) <- "alienData"
-    res
+  
 }
