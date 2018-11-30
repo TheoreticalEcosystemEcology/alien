3fc5d48ccb9d3eb07c8195fb76330d3f2f7a1640
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-11-30 11:57:08 -0500

    Adapted fit4corner to qork with the new alienData and included mvabund

--- R/fit4corner.R
+++ R/fit4corner.R
@@ -2,19 +2,15 @@
 #'
 #' @title Fit fourth corner prediction model
 #'
-#' @description Fit fourth corner prediction model using either the approach proposed by Brown et al. (2014) or Ovaskainen et al. (2017)
+#' @description Fit fourth corner prediction model using either the approach proposed by Brown et al. (2014) (\code{\link[mvabund]{traitglm}}) or Ovaskainen et al. (2017) (\link[HMSC]{hmsc}).
 #'
 #' @param data an object of the class alienData, see as.alienData function.
 #' @param formulaFrom formula to construct the trait variables associated to the "From" organisms. Default is "~ .".
 #' @param formulaTo formula to construct the trait variables associated to the "To" organisms. Default is "~ .".
-#' @param level Either "species" or "individuals". Whether the analysis should be done at the species or the individual levels. Default is "individual" (FOR NOW!). (See details)
-#' @param class method to be applied on the data. Either 'mvabund' or 'HMSC' for the implementation of the 'mvabund' (\link[mvabund]{traitglm}) or the 'HMSC' (\link[HMSC]{hmsc}) R package, respectively.
+#' @param type method to be applied on the data. Either 'mvabund' or 'HMSC' for the implementation of the 'mvabund' (\link[mvabund]{traitglm}) or the 'HMSC' (\link[HMSC]{hmsc}) R package, respectively.
 #' @param family For 'mvabund', the family of the response variable can be defined using \link[stats]{family}. The negative binomial with unknown overdispersion and a log-link can be specified as "negative.binomial", and it is the default. For 'HMSC', use either 'probit', 'logit', 'gaussian', 'poisson', 'overPoisson'.
-#' @param priors An object of class \code{\link[HMSC]{HMSCpriors}}. If NULL, the function will generate flat priors to estimate the model. This argument is active only when "HMSC" is used.
-#' @param iniParam An object of class \code{\link[HMSC]{HMSCparam}}. If NULL, the function will generate initial parameters ramdomly. This argument is active only when "HMSC" is used.
-#' @param niter A positive integer defining the total number of iterations to be carried out in the analysis. Default is 2000. This argument is active only when "HMSC" is used.
-#' @param nburn A positive integer defining the number of iterations to be used in the burning (first) phase of the algorithm. The burning iterations are a fraction of \code{niter}. This argument is active only when "HMSC" is used.
-#' @param thin A positive integer defining thinning. Default is 1 (see details in \link[HMSC]{hmsc}).
+#' @param priors An object of class \code{HMSCprior} (\code{\link[HMSC]{as.HMSCprior}}). If NULL, the function will generate flat priors to estimate the model. This argument is active only when "HMSC" is used.
+#' @param iniParam An object of class \code{HMSCparam} (\code{\link[HMSC]{as.HMSCparam}}). If NULL, the function will generate initial parameters ramdomly. This argument is active only when "HMSC" is used.
 #' @param \dots Other parameters passed to either \link[mvabund]{traitglm} or \link[HMSC]{hmsc}.
 #' 
 #' @details 
@@ -25,11 +21,14 @@
 #' 
 #' The arguments \code{formulaFrom} and \code{formulaTo} should take the form \code{~ x + y * z}, that is, the left side of the equation should not be given. Also, note that the default formulas always include an intercept.
 #' 
-#' The argument \code{level} takes into account species if the data considered has multiple measures for the same species. In this case the mean (numeric variable) or the dominant level (factor) will be considered when gathering the data by species.
+#' @return
+#' 
+#' An object of class hmsc (for the analysis carried out with the HMSC package) or an object of class traitglm (for the analysis carried out with the mvabund package).
 #' 
 #' @author
 #' F. Guillaume Blanchet
 #' 
+#' @importFrom stats model.matrix
 #' @importFrom mvabund traitglm
 #' @importFrom HMSC as.HMSCdata
 #' @importFrom HMSC as.HMSCprior
@@ -39,9 +38,10 @@
 #' @export
 
 
-fit4corner <- function(data, formulaFrom = "~ .", formulaTo = "~ .",
-                       level = "individual", class = "HMSC",
-                       family = NULL, priors = NULL, iniParam = NULL, ...){
+fit4corner <- function(data, formulaFrom = "~ .", 
+                       formulaTo = "~ .", type = "HMSC", 
+                       family = NULL, priors = NULL, 
+                       iniParam = NULL, ...){
   # General check
   stopifnot(class(data) == "alienData")
   
@@ -49,41 +49,46 @@ fit4corner <- function(data, formulaFrom = "~ .", formulaTo = "~ .",
   # Format data
   #############
   # Adjacency matrix
-  adjData <- getAdjacencyMatrix(data, level = level, bipartite = TRUE)
+  adjData <- getAdjacencyMatrix(data, bipartite = TRUE)
   
   # "Raw " trait data
-  traits <- getTraitMatrix(data, level = level, bipartite = TRUE)
-  
-  # Trait data from
-  traitsFrom <- model.matrix(as.formula(formulaFrom), data = traits$from)
-  
-  # Trait data to
-  traitsTo <- model.matrix(as.formula(formulaTo), data = traits$to)
+  traits <- getTraitMatrix(data, bipartite = TRUE)
   
   # mvabund
   if(class == "mvabund"){
-#    res <- mvabund::traitglm(adjData, traitsFrom, traitsTo,
-#                             family = family, ...)
-    stop("The implementation with 'mvabund' is not yet implemented... it will come soon... I hope")
+    # Perform traitglm analysis
+    res <- mvabund::traitglm(adjData, traits$to, traits$from,
+                             family = family, ...)
   }
 
   # HMSC
   if(class == "HMSC"){
-    Data <- HMSC::as.HMSCdata(Y = adjData, X = traitsFrom,
-                              Tr = t(traitsTo), scaleX = FALSE,
+    # Trait data from
+    traitsFrom <- stats::model.matrix(as.formula(formulaFrom), data = traits$from)
+    
+    # Trait data to
+    traitsTo <- stats::model.matrix(as.formula(formulaTo), data = traits$to)
+    
+    # Construct HMSCdata object
+    Data <- HMSC::as.HMSCdata(Y = adjData, X = traitsTo,
+                              Tr = t(traitsFrom), scaleX = FALSE,
                               scaleTr = FALSE,
                               interceptX = FALSE,
                               interceptTr = FALSE)
+
+    # Construct HMSCprior object
     if(is.null(priors)){
       priors <- HMSC::as.HMSCprior(Data, family = family)
     }
+    
+    # Construct HMSCprior object
     if(is.null(iniParam)){
       iniParam <- HMSC::as.HMSCparam(Data, priors)
     }
       
+    # Perform HMSC analysis
     res <- HMSC::hmsc(Data, param = iniParam, priors = priors,
-                      family = family, niter = niter,
-                      nburn = nburn, thin = thin, ...)
+                      family = family, ...)
   }
   
   return(res)
