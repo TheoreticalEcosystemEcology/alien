fe4e9beeb2f5e8d01dc343ada46deae31a0fee13
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-12-04 10:26:40 -0500

    add webFromNicheModel

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -41,11 +41,27 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// webFromNicheModel
+LogicalMatrix webFromNicheModel(int nbsp, double connec, bool connect_all, bool unbias, Nullable<NumericVector> niche);
+RcppExport SEXP _alien_webFromNicheModel(SEXP nbspSEXP, SEXP connecSEXP, SEXP connect_allSEXP, SEXP unbiasSEXP, SEXP nicheSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type nbsp(nbspSEXP);
+    Rcpp::traits::input_parameter< double >::type connec(connecSEXP);
+    Rcpp::traits::input_parameter< bool >::type connect_all(connect_allSEXP);
+    Rcpp::traits::input_parameter< bool >::type unbias(unbiasSEXP);
+    Rcpp::traits::input_parameter< Nullable<NumericVector> >::type niche(nicheSEXP);
+    rcpp_result_gen = Rcpp::wrap(webFromNicheModel(nbsp, connec, connect_all, unbias, niche));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_alien_getNull", (DL_FUNC) &_alien_getNull, 1},
     {"_alien_getNullOne", (DL_FUNC) &_alien_getNullOne, 1},
     {"_alien_prodNorm", (DL_FUNC) &_alien_prodNorm, 3},
+    {"_alien_webFromNicheModel", (DL_FUNC) &_alien_webFromNicheModel, 5},
     {NULL, NULL, 0}
 };
 
