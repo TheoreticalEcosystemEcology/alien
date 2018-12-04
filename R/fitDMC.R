424769dffd06c4f606f678032b789293be3e4264
Guillaume Blanchet <guiblanchet3@gmail.com>
2018-12-04 14:31:27 -0500

    fitDMC now works

--- R/fitDMC.R
+++ R/fitDMC.R
@@ -4,29 +4,35 @@
 #'
 #' @description Fit direct matching centrality model
 #'
-#' @param formula formula to define the model.
+#' @param formula A one-sided formula specifying how the different traits should be used to estimate species interactions.
 #' @param data an object of the class \code{\link{alienData}}
-#' @param class Method to use to estimate the model. Either "randomForest" (\link[randomForest]{randomForest}) or "glm" (\code{\link[stats]{glm}}). 
+#' @param binary Logical. Whether the adjacency matrix is binary or not. Default is TRUE.
+#' @param type Method to use to estimate the model. Either "randomForest" (\link[randomForest]{randomForest}) or "glm" (\code{\link[stats]{glm}}). 
 #' @param family The family of the response variable. See \link[stats]{family}, or the choices available.
 #' @param \dots Other parameters passed to either \link[randomForest]{randomForest} or \link[stats]{glm}.
+#' 
+#' @details 
+#' 
+#' The direct matching centrality models are designed to be used on bipartite network where traits are available for both sets of species interacting in the network. It should not be used otherwise.
+#' 
+#' This function builds the adjacency matrix, unfold it and uses it as the response variables. As explanatory variables, the traits for each sets of species are repeated to match the length of the unfolded adjacency matrix but also the position.
+#' 
+#' @return 
+#' 
+#' A species by species matrix.
 #' 
 #' @author
 #' 
 #' Dominique Gravel, Steve Vissault, F. Guillaume Blanchet
 #'
-#' @importFrom stats glm
-#' @importFrom randomForest randomForest
-#' @importFrom stats aggregate as.formula
-#' @importFrom utils type.convert
-#'
 #' @export
-fitDMC <- function(formula, data, binary = TRUE, class = NULL, family = NULL, 
-                   traits = NULL, ...) {
+fitDMC <- function(formula, data, binary = TRUE, type = NULL,
+                   family = NULL, ...) {
 
   stopifnot(class(data) == "alienData")
 
   # Construct adjencency matrix
-  adjMat <- getAdjacencyMatrix(data, bipartite = TRUE)
+  adjMat <- getAdjacencyMatrix(data, bipartite = TRUE, binary = TRUE)
   nFromSp <- ncol(adjMat)
   nToSp <- nrow(adjMat)
   
@@ -49,5 +55,30 @@ fitDMC <- function(formula, data, binary = TRUE, class = NULL, family = NULL,
   traitsFrom <- as.data.frame(traits$from[rep(seq_len(nFromSp), each = nToSp),])
   colnames(traitsFrom) <- colnames(traits$from)
   
+  # organize data into a single object
   dat <- cbind(adjVec, traitsTo, traitsFrom)
+  
+  # Reorganize formula
+  Formula <- adjVec ~ x + y
+  Formula[[1]] <- formula[[1]]
+  Formula[[3]] <- formula[[2]]
+  
+  # GLM
+  if(type == "glm"){
+    model <- stats::glm(Formula, data = dat, family = family)
+  }
+
+  # Random forest
+  if(type == "randomForest"){
+    model <- randomForest::randomForest(Formula, data = dat)
+  }
+  
+  # Prediction
+  pred <- predict(model, type = "response")
+  
+  # Organise result into a matrix
+  res <- matrix(pred, nrow = nToSp, ncol = nFromSp)
+  
+  # Return results
+  return(res)
 }
