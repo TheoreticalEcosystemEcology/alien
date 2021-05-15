e63377560eacc131cce57eb05c1eb8844ec645ef
guiblanchet <guiblanchet3@gmail.com>
2021-05-14 21:07:50 -0400

    Add vignette and data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,15 @@
-#' data name
+#' Salix - Galler - parasitoids data 
 #'
-#' data description 
+#' The Salix - galler - parasitoids data was originally published by Kopelke et al. (2017) for which additional information on phylogeny was added. The full data was used to perform the analysis in Wooton et al. (2021).
 #'
 #' @docType data
 #' @keywords datasets
-#' @name bogus
-#' @usage bogus
-#' @format 2beadded
-#"bogus"
+#' @name salixGal
+#' @usage data(salixGal)
+#' @usage data(galPara)
+#' 
+#' @format an object of class alienData
+#' 
+#' @references 
+#' 
+NULL
